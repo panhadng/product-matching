a32e3de8ef30f25ce6ebb412c7e4370a821b5f34
Akshay Chavan <39055315+akshaychavan8@users.noreply.github.com>
2024-10-15 17:14:48 +1100

    Update describe2.py
    using a different model

--- models/describe2.py
+++ models/describe2.py
@@ -20,6 +20,46 @@ from io import BytesIO
 # Suppress warnings
 warnings.filterwarnings("ignore")
 
+def describe_image_enhanced_1(image_path):
+    """
+    Generate an enhanced caption for an image using the BLIP model.
+
+    Args:
+        image_path (str): The path to the image file.
+
+    Returns:
+        str: A generated caption describing the image.
+
+    This function uses the BLIP-2 (Salesforce/blip-image-captioning-large) model
+    to generate a more detailed caption for the given image.
+    """
+
+    # Load the image and convert it to RGB
+    image = Image.open(image_path).convert('RGB')
+  
+    # The API requires the image to be converted to a byte format and image for sending
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    image_bytes = buffered.getvalue()
+   
+    # Send the image to the Hugging Face Inference API using URL nd APIKey
+    API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large"
+    headers = {"Authorization": "Bearer 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"}
+    
+    # Send the request
+    response = requests.post(API_URL, headers=headers, data=image_bytes)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the response and return the description
+        result = response.json()
+        caption = result[0]['generated_text']
+        print(caption)
+        return caption
+    
+    else:
+        # Handle the error
+        raise Exception(f"Failed to get description. Status code: {response.status_code}")
 
 
 # ------------------------------------------
