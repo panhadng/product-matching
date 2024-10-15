d52907c1ebd85d26987d62b0e1f2bcc337143204
Akshay Chavan <akshaychavan8@gmail.com>
2024-10-15 17:21:10 +1100

    added another model tp describe 2 added code in test.py to test

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
-from models.describe import describe_image_simple
+# from models.describe import describe_image_simple
+from models.describe2 import describe_image_enhanced_1
 
 
 def main():
-    image_path = "models/images/keyboard1.jpg"
-    description = describe_image_simple(image_path)
+    # image_path = "models/images/keyboard1.jpg"
+    # description = describe_image_simple(image_path)
+    # print(f"Image description: {description}")
+
+    # To Test describe2. that uses APIURL and APIKEY
+    image_path = "models/images/keyboard2.jpg"
+    # image_path = "models/images/chair3.jpg"
+    description = describe_image_enhanced_1(image_path)
     print(f"Image description: {description}")
 
 
